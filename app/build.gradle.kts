d550639a0efe897c8a7c438f3431e2f64b90aa0a
Pierce <123277032+0Pierce@users.noreply.github.com>
2024-02-29 23:22:35 -0500

    Added LiveViewCatalogue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("io.github.sceneview:arsceneview:2.0.3")
+    implementation ("com.google.ar:core:1.41.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewCatalogue.kt
@@ -0,0 +1,12 @@
+package com.exceptionhandlers.avante_ar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LiveViewCatalogue : Fragment(R.layout.fragment_live_view_catalogue) {
+
+}
