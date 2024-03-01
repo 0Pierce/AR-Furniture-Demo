2ba12a422632753259b5ae55af978463f7239a0b
Pierce <123277032+0Pierce@users.noreply.github.com>
2024-03-01 02:47:48 -0500

    Added catalogue menu fragment
    - Can open and close catalogue fragment in liveView Activity

--- app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewActivity.kt
+++ app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewActivity.kt
@@ -1,9 +1,14 @@
 package com.exceptionhandlers.avante_ar
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.view.isVisible
+import androidx.fragment.app.FragmentContainerView
+
 /*
 *Name: LiveViewActivity
 *
@@ -29,7 +34,7 @@ import android.widget.Button
 class LiveViewActivity : AppCompatActivity() {
 
 
-    lateinit var catalogue: LiveViewCatalogue
+    lateinit var catalogue: FragmentContainerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_live_view)
@@ -40,6 +45,23 @@ class LiveViewActivity : AppCompatActivity() {
         btnBack.setOnClickListener{
             startActivity(Intent(this, HomePageActivity::class.java))
 
+        }
+
+        catalogue=findViewById(R.id.catalogueFragment)
+        val btnCatOpen = findViewById<ImageButton>(R.id.imgBtnCatOpen)
+        val btnCatClose = findViewById<ImageButton>(R.id.imgBtnCatClose)
+        catalogue.isVisible = false
+
+        btnCatClose.scaleX = -1f
+
+        btnCatOpen.setOnClickListener{
+            catalogue.isVisible = true
+            btnCatClose.isVisible = true
+        }
+
+        btnCatClose.setOnClickListener{
+            catalogue.isVisible = false
+            btnCatClose.isVisible = false
         }
 
     }
