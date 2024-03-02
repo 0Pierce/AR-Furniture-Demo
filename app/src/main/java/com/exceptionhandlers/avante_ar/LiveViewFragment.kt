d30c1dd1ea94a592edc41a3b03e63c5753314486
Pierce <123277032+0Pierce@users.noreply.github.com>
2024-03-02 00:36:15 -0500

    Implemented basic plane detection using sample code

--- app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewFragment.kt
+++ app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewFragment.kt
@@ -6,14 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isGone
+import androidx.lifecycle.lifecycleScope
 import com.google.android.filament.Viewport
+import com.google.ar.core.Anchor
 import io.github.sceneview.ar.ARSceneView
 import com.google.ar.core.Config
 import com.google.ar.core.Session
 import com.google.ar.core.Plane
-
-
-
+import com.google.ar.core.TrackingFailureReason
+import io.github.sceneview.ar.arcore.getUpdatedPlanes
+import io.github.sceneview.ar.getDescription
+import io.github.sceneview.ar.node.AnchorNode
+import io.github.sceneview.math.Position
+import io.github.sceneview.node.ModelNode
+import kotlinx.coroutines.launch
 
 
 /*
@@ -41,12 +49,42 @@ import com.google.ar.core.Plane
 class LiveViewFragment : Fragment(R.layout.fragment_live_view)   {
 
     lateinit var sceneViewPort: ARSceneView
+    lateinit var loadingView: View
+    lateinit var instructionText: TextView
+    var isLoading = false
+        set(value) {
+            field = value
+            loadingView.isGone = !value
+        }
 
+    var anchorNode: AnchorNode? = null
+        set(value) {
+            if (field != value) {
+                field = value
+                updateInstructions()
+            }
+        }
 
+    var trackingFailureReason: TrackingFailureReason? = null
+        set(value) {
+            if (field != value) {
+                field = value
+                updateInstructions()
+            }
+        }
+
+    fun updateInstructions() {
+        instructionText.text = trackingFailureReason?.let {
+            it.getDescription(requireContext())
+        } ?: if (anchorNode == null) {
+            getString(R.string.point_your_phone_down)
+        } else {
+            null
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
 
     }
@@ -54,12 +92,77 @@ class LiveViewFragment : Fragment(R.layout.fragment_live_view)   {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        instructionText = view.findViewById(R.id.instructionText)
+        loadingView = view.findViewById(R.id.loadingView)
+        sceneViewPort = view.findViewById<ARSceneView?>(R.id.sceneViewLive).apply {
+            planeRenderer.isEnabled = true
+            configureSession { session, config ->
+                config.depthMode = when (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+                    true -> Config.DepthMode.AUTOMATIC
+                    else -> Config.DepthMode.DISABLED
+                }
+                config.instantPlacementMode = Config.InstantPlacementMode.DISABLED
+                config.lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
+            }
+            onSessionUpdated = { _, frame ->
+                if (anchorNode == null) {
+                    frame.getUpdatedPlanes()
+                        .firstOrNull { it.type == Plane.Type.HORIZONTAL_UPWARD_FACING }
+                        ?.let { plane ->
+                            addAnchorNode(plane.createAnchor(plane.centerPose))
+                        }
+                }
+            }
+            onTrackingFailureChanged = { reason ->
+                this@LiveViewFragment.trackingFailureReason = reason
+            }
+        }
 
-        sceneViewPort = view.findViewById(R.id.sceneViewLive)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
     }
 
+    fun addAnchorNode(anchor: Anchor) {
+        sceneViewPort.addChildNode(
+            AnchorNode(sceneViewPort.engine, anchor)
+                .apply {
+                    isEditable = true
+                    lifecycleScope.launch {
+                        isLoading = true
+                        sceneViewPort.modelLoader.loadModelInstance(
+                            "https://sceneview.github.io/assets/models/DamagedHelmet.glb"
+                        )?.let { modelInstance ->
+                            addChildNode(
+                                ModelNode(
+                                    modelInstance = modelInstance,
+                                    // Scale to fit in a 0.5 meters cube
+                                    scaleToUnits = 0.5f,
+                                    // Bottom origin instead of center so the model base is on floor
+                                    centerOrigin = Position(y = -0.5f)
+                                ).apply {
+                                    isEditable = true
+                                }
+                            )
+                        }
+                        isLoading = false
+                    }
+                    anchorNode = this
+                }
+        )
+    }
 
 }
