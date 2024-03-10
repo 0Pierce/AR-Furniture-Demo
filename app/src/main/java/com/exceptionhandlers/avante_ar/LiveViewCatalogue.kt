8f6505b98f0e83c87398b3ca0c868d633f0e345d
Isaac Zeng <izeng335612016lol@gmail.com>
2024-03-10 16:12:14 -0400

    Avante-AR
    Adding catolgue to the script (Probably need to get models)

--- app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewCatalogue.kt
+++ app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewCatalogue.kt
@@ -5,8 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import java.lang.reflect.Modifier
 
 
 class LiveViewCatalogue : Fragment(R.layout.fragment_live_view_catalogue) {
 
 }
+
+// Need to add drawable items
+// Add item set for arrows once I get back home
+// Create New assets folder for assets + add Android Resource Directory (models)
+// Paste assets into the folder (need to get our assets) (shelf, lamp, bed, cabinet, etc)
+// Drawable files with the models too (ex.. burger.webp, shelf.webp, lamp.webp)
+
+@Composable
+fun Menu(modifer: Modifier){
+    val itemsList = listOf(
+        Furniture(name:"shelf", R.drawable.shelf)
+        Furniture(name:"lamp", R.drawable.lamp)
+        Furniture(name:"bed", R.drawable.bed)
+        Furniture(name:"cabinet", R.drawable.cabinet)
+        Furniture(name:"mirror", R.drawable.mirror)
+    )
+    Row(modifier = modifier.fillMaxWidth(),
+        verticalAlightment = Alignment.CenterVertically, {this:Rowscope}
+        
+    }
+}
+
+data class Furniture(var name:String, var imageID:Int)
