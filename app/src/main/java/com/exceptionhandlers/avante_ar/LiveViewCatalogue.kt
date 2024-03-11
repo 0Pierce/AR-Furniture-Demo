938baf7f2c4402d928a6f02115b80aa02a1788e7
Isaac Zeng <izeng335612016lol@gmail.com>
2024-03-10 20:33:16 -0400

    update
    update

--- app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewCatalogue.kt
+++ app/src/main/java/com/exceptionhandlers/avante_ar/LiveViewCatalogue.kt
@@ -14,9 +14,6 @@ class LiveViewCatalogue : Fragment(R.layout.fragment_live_view_catalogue) {
             Furniture(name ="shelf", imageID = R.drawable.shelf, path = "drawable/shelf"),
             Furniture(name ="lamp", imageID = R.drawable.sofa, path = ""),
             Furniture(name ="bed", imageID = R.drawable.bed, path = ""),
-            Furniture(name ="cabinet", imageID = R.drawable.cabinet, path = ""),
-            Furniture(name ="mirror", imageID = R.drawable.mirror, path = ),
-
 }
 
     data class Furniture(var name:String, var imageID:Int, var path: String)
