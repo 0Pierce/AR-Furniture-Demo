555a6c93fb01130f7a474c02ef322d85744a3af0
h4h4h1h1 <yx2wang@uwaterloo.ca>
2024-03-14 00:48:01 -0400

    continued corrections to confidencetree (kotlin is certainly built different)

--- app/src/main/java/com/exceptionhandlers/avante_ar/testing_range/ConfidenceTree.kt
+++ app/src/main/java/com/exceptionhandlers/avante_ar/testing_range/ConfidenceTree.kt
@@ -1,73 +1,55 @@
-//going to try making a modified quadrant tree for confidence points
-//was originally going for a kd-tree, but the quadrant tree would probably be less confusing to use
+import kotlin.math.abs
+
+//going to try making a modified oct-tree for confidence points
+//was originally going for a kd-tree, but the oct-tree would probably be less confusing to use
 //the largest unit will be 1m blocks
 
-internal class Point {
-    protected var x : Double
-    protected var y : Double
-    protected var z : Double
-    protected var confidence : Double = 0.0
+internal class Point(x1: Double, y1: Double, z1: Double, c: Double) {
+    protected var x : Double = x1
+    protected var y : Double = y1
+    protected var z : Double = z1
+    protected var confidence : Double = c
 
-     Point(var x1: Double, var y1: Double, var z1: Double, var c: Double) {
-
-
-
-            this.x = x1
-            this.y = y1
-            this.z = z1
-            this.confidence = c
-
-    }
-    fun getCoords(): Triple<Double> {
+    fun getCoords(): Triple<Double, Double, Double> {
         return Triple(this.x, this.y, this.z)
     }
     fun getConf(): Double {
         return this.confidence
     }
-    fun compareCoords(var p2: Point, var res: Double) {
+    fun compareCoords(p2: Point, res: Double): Boolean {
         return (abs(this.x-p2.x) <= res && abs(this.y-p2.y) <= res && abs(this.z-p2.z) <= res)
     }
-    fun compareConf(var p2: Point, var res: Double) {
+    fun compareConf(p2: Point, res: Double) : Boolean {
         return abs(this.confidence-p2.confidence) <= res
     }
 }
 
-internal class Chunk {
-    protected val size = 0 //number of points
+internal class Chunk(sl: Double, og: Triple<Double, Double, Double>, res: Double, cres: Double){
+    protected var size = 0 //number of points
 
-    protected val leaf = true //will always be a leaf on init, until split further
+    protected var leaf = true //will always be a leaf on init, until split further
 
-    protected val sideLength : Double
-    protected val origin: Triple<Double> // coordinate with the least x/y/z possible in a chunk
+    protected var sideLength : Double = sl
+    protected var origin: Triple<Double, Double, Double> = og // coordinate with the least x/y/z possible in a chunk
 
-    protected val elements : Vector<Point> //always keeps track of what elements are inside
+    protected var elements = ArrayList<Point>() //always keeps track of what elements are inside
 
-    protected val normConfidence = 0 //average of all contained confidence points
-    protected val nc_updated = true //we will use lazy updating, if it isn't updated, it will be updated when the user next wants this value
+    protected var normConfidence = 0 //average of all contained confidence points
+    protected var nc_updated = true //we will use lazy updating, if it isn't updated, it will be updated when the user next wants this value
 
-    protected val confidenceDensity = 0 //sum of all confidence points divided by the volume of the chunk
-    protected val cd_updated = true //on init, the values ARE updated, as there are no points inside currently
+    protected var confidenceDensity = 0 //sum of all confidence points divided by the volume of the chunk
+    protected var cd_updated = true //on init, the values ARE updated, as there are no points inside currently
 
-    protected val quadrants : Array<Array<Chunk>> = arrayOf(arrayOf(Null, Null), arrayOf(Null, Null), arrayOf(Null, Null), arrayOf(Null, Null)) //sub-quadrants, if any
+    protected var octants : Array<Array<Chunk?>> = arrayOf(arrayOf(null, null), arrayOf(null, null), arrayOf(null, null), arrayOf(null, null)) //sub-octants, if any
 
-    protected val resolution : Double //if we compare 2 points, how close is close enough?
-    protected val confres : Double //if we compare 2 points by confidence, how close is close enough?
+    protected var resolution : Double = res //if we compare 2 points, how close is close enough?
+    protected var confres : Double = cres //if we compare 2 points by confidence, how close is close enough?
 
-
-    class Chunk constructor(var sl: Double, var og: Triple<Double>, var res: Double, var cres: Double) {
-        init {
-            this.sideLength = sl
-            this.origin = og
-            this.resolution = res
-            this.confres = cres
-        }
-    }
-
-    fun insert(var p: Point) {
+    fun insert(p: Point) {
 
     }
 
-    fun delete(var p: Point) { //delete by confidence AND point compare
+    fun delete(p: Point) { //delete by confidence AND point compare
 
     }
     protected fun split() { //initializes sub-quadrants, and inserts existing values into them
@@ -76,5 +58,5 @@ internal class Chunk {
 }
 
 class ConfidenceTree {
-    protected val meterChunks = LinkedHashMap<Triple<Int, Int, Int>, Chunk>() //chunks in this outer layer are a meter cube
+    private var meterChunks = HashMap<Triple<Int, Int, Int>, Chunk>() //chunks in this outer layer are a meter cube
 }
